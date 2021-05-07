905f30af6c225f6a5f0ff8fa81fbc0537b00702f
virigos <czmaneu@gmail.com>
2021-05-07 10:27:08 +0200

    zmena

--- ProjektSkola0705/Form1.cs
+++ ProjektSkola0705/Form1.cs
@@ -19,7 +19,7 @@ namespace ProjektSkola0705
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ahoj");
+            MessageBox.Show("ahoj"); //blbost
         }
     }
 }
